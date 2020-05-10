97d91e2427165d0f71d8f8589913e15399285cd4
Sabine Schilling <sabineschilling@gmx.ch>
2020-05-11 00:28:03 +0200

    added testhat

---
+++ tests/testthat/Examples_from_README.R
@@ -0,0 +1,20 @@
+##Examples------ 
+#Trees data set----
+visstat(trees,"Girth","Height") #without saving of plot
+
+#Iris data set----
+visstat(iris,"Petal.Width", "Species")
+visstat(iris,"Petal.Width", "Petal.Length")
+
+#InsectSprays  data set----
+visstat(InsectSprays,"count","spray")
+
+#Titanic data set---- 
+#install.packages("titanic")
+library(titanic)
+titanic_train$Survived=as.factor(titanic_train$Survived)
+titanic_train$Pclass=as.factor(titanic_train$Pclass)
+visstat(titanic_train,"Survived","Pclass")
+
+
+#HairEyeColorc data set---- 
