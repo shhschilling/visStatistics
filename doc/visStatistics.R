18bcf9211d86f8bcb3530789a6b5ec79243b107f
Sabine Schilling <shhschilling@users.noreply.github.com>
2025-05-05 12:36:13 +0200

    Add pkgdown site

--- doc/visStatistics.R
+++ doc/visStatistics.R
@@ -1,10 +1,11 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>",
-  fig.width = 8,
-  fig.height = 5,
-  out.width = "100%"
+ echo = TRUE,
+ collapse = TRUE,
+ comment = "#>",
+ fig.width = 8,
+ fig.height = 5,
+ out.width = "100%"
 )
 
 ## ----setup--------------------------------------------------------------------
@@ -12,35 +13,33 @@ library(visStatistics)
 
 ## -----------------------------------------------------------------------------
 mtcars$am <- as.factor(mtcars$am)
-ttestStatistics <- visstat(mtcars, "mpg", "am")
-# Print the test statistics
-ttestStatistics
+t_test_statistics <- visstat(mtcars, "mpg", "am")
+# t_test_statistics # Uncomment this line to print out the test statistics
 
 ## -----------------------------------------------------------------------------
-# Set the confidence level explicitly resulting in wider confidence levels
 mtcars$am <- as.factor(mtcars$am)
-ttestStatistics <- visstat(mtcars, "mpg", "am", conf.level = 0.99)
+t_test_statistics_99 <- visstat(mtcars, "mpg", "am", conf.level = 0.99)
 
 ## -----------------------------------------------------------------------------
 grades_gender <- data.frame(
-  Sex = as.factor(c(rep("Girl", 21), rep("Boy", 23))),
-  Grade = c(
-    19.3, 18.1, 15.2, 18.3, 7.9, 6.2, 19.4,
-    20.3, 9.3, 11.3, 18.2, 17.5, 10.2, 20.1, 13.3, 17.2, 15.1, 16.2, 17.0,
-    16.5, 5.1, 15.3, 17.1, 14.8, 15.4, 14.4, 7.5, 15.5, 6.0, 17.4,
-    7.3, 14.3, 13.5, 8.0, 19.5, 13.4, 17.9, 17.7, 16.4, 15.6, 17.3, 19.9, 4.4, 2.1
-  )
+sex = as.factor(c(rep("girl", 21), rep("boy", 23))),
+grade = c(
+19.3, 18.1, 15.2, 18.3, 7.9, 6.2, 19.4,
+20.3, 9.3, 11.3, 18.2, 17.5, 10.2, 20.1, 13.3, 17.2, 15.1, 16.2, 17.0,
+16.5, 5.1, 15.3, 17.1, 14.8, 15.4, 14.4, 7.5, 15.5, 6.0, 17.4,
+7.3, 14.3, 13.5, 8.0, 19.5, 13.4, 17.9, 17.7, 16.4, 15.6, 17.3, 19.9, 4.4, 2.1
+)
 )
 
-wilcoxonStatistics <- visstat(grades_gender, "Grade", "Sex")
+wilcoxon_statistics <- visstat(grades_gender, "grade", "sex")
 
 ## -----------------------------------------------------------------------------
 oneway_npk <- visstat(npk, "yield", "block")
 
 ## -----------------------------------------------------------------------------
-InsectSprays_tr <- InsectSprays
-InsectSprays_tr$count <- sqrt(InsectSprays$count)
-visstat(InsectSprays_tr, "count", "spray")
+insect_sprays_tr <- InsectSprays
+insect_sprays_tr$count_sqrt <- sqrt(InsectSprays$count)
+visstat(insect_sprays_tr, "count_sqrt", "spray")
 
 ## -----------------------------------------------------------------------------
 visstat(iris, "Petal.Width", "Species")
@@ -50,9 +49,6 @@ linreg_cars <- visstat(cars, "dist", "speed")
 
 ## -----------------------------------------------------------------------------
 linreg_cars <- visstat(cars, "dist", "speed", conf.level = 0.99)
-# Extract the test statistics
-linreg_cars$anderson_darling_test_residuals
-linreg_cars$shapiro_test_residuals
 
 ## -----------------------------------------------------------------------------
 linreg_cars <- visstat(trees, "Height", "Girth", conf.level = 0.9)
@@ -61,22 +57,30 @@ linreg_cars <- visstat(trees, "Height", "Girth", conf.level = 0.9)
 HairEyeColorDataFrame <- counts_to_cases(as.data.frame(HairEyeColor))
 
 ## -----------------------------------------------------------------------------
-HairEyeColorDataFrame <- counts_to_cases(as.data.frame(HairEyeColor))
-visstat(HairEyeColorDataFrame, "Hair", "Eye")
+hair_eye_color_df <- counts_to_cases(as.data.frame(HairEyeColor))
+visstat(hair_eye_color_df, "Hair", "Eye")
 
 ## -----------------------------------------------------------------------------
-HairEyeColorMaleFisher <- HairEyeColor[, , 1]
-# slicing out a 2 x2 contingency table
-blackBrownHazelGreen <- HairEyeColorMaleFisher[1:2, 3:4]
-blackBrownHazelGreen <- counts_to_cases(as.data.frame(blackBrownHazelGreen))
-fisher_stats <- visstat(blackBrownHazelGreen, "Hair", "Eye")
-# fisher_stats  #uncommenting prints out summary statistics
+hair_black_brown_eyes_brown_blue <- HairEyeColor[1:2, 1:2, ]
+#Transform to data frame
+hair_black_brown_eyes_brown_blue_df<- counts_to_cases(as.data.frame(hair_black_brown_eyes_brown_blue))
+#Chi-squared test
+visstat(hair_black_brown_eyes_brown_blue_df, "Hair", "Eye")
 
 ## -----------------------------------------------------------------------------
-visstat(blackBrownHazelGreen, "Hair", "Eye",
-  graphicsoutput = "png", plotDirectory =
+hair_eye_color_male <- HairEyeColor[, , 1]
+# Slice out a 2 by 2 contingency table
+black_brown_hazel_green_male <- hair_eye_color_male[1:2, 3:4]
+#Transform to data frame
+black_brown_hazel_green_male <- counts_to_cases(as.data.frame(black_brown_hazel_green_male))
+# Fisher test
+fisher_stats <- visstat(black_brown_hazel_green_male, "Hair", "Eye")
 
-    tempdir()
+## -----------------------------------------------------------------------------
+visstat(black_brown_hazel_green_male, "Hair", "Eye",
+graphicsoutput = "png", plotDirectory =
+
+tempdir()
 )
 
 ## -----------------------------------------------------------------------------
