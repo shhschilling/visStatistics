eb9f655f1db14bc200cc60ec5ab0a307a76a5255
Sabine Schilling <shhschilling@users.noreply.github.com>
2025-05-09 00:07:33 +0200

    vignette with decision tree" git add .

---
+++ tools/toggle_vignette_mode.R
@@ -0,0 +1,45 @@
+toggle_vignette_mode <- function(file = "vignettes/visStatistics.Rmd", mode = c("cran", "dev")) {
+  mode <- match.arg(mode)
+
+  common_fields <- c(
+    'title: "visStatistics"',
+    'author: "Sabine Schilling"',
+    'date: "`r Sys.Date()`"',
+    'bibliography: visstat.bib',
+    'vignette: >',
+    '  %\\VignetteIndexEntry{visStatistics}',
+    '  %\\VignetteEngine{knitr::rmarkdown}',
+    '  %\\VignetteEncoding{UTF-8}',
+    'editor_options:',
+    '  markdown:',
+    '    wrap: sentence'
+  )
+
+  output_block <- switch(mode,
+    cran = c(
+      "output:",
+      "  html_vignette:",
+      "    toc: true",
+      "    number_sections: true",
+      "    toc_depth: 3"
+    ),
+    dev = c(
+      "output:",
+      "  bookdown::html_document2:",
+      "    toc: true",
+      "    number_sections: true",
+      "    toc_depth: 3"
+    )
+  )
+
+  new_yaml <- c("---", output_block, common_fields, "---")
+
+  lines <- readLines(file)
+  delim_indices <- which(trimws(lines) == "---")
+  if (length(delim_indices) < 2) stop("Could not find complete YAML block.")
+
+  content <- lines[(delim_indices[2] + 1):length(lines)]
+  writeLines(c(new_yaml, content), con = file)
+
+  message("📄 YAML header updated to ", mode, " mode.")
+}
