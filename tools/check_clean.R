570564dce2d4311bf8ab4feb2e753ff4a1c43d85
Sabine Schilling <shhschilling@users.noreply.github.com>
2025-05-10 11:18:45 +0200

    updated Readme"

--- tools/check_clean.R
+++ tools/check_clean.R
@@ -1,16 +1,16 @@
 check_clean <- function(pkg = ".", ...) {
   toggle_vignette_mode(mode = "dev")
   
-  message("🧪 Running devtools::check() with --as-cran and compact vignettes...")
+  message("???? Running devtools::check() with --as-cran and compact vignettes...")
   devtools::check(pkg = pkg, args = "--as-cran", build_args = "--compact-vignettes=qpdf", ...)
   
-  message("🧹 Removing .DS_Store files...")
+  message("???? Removing .DS_Store files...")
   system("find . -name '.DS_Store' -delete")
   
-  message("🧼 Removing *.Rcheck directories...")
+  message("???? Removing *.Rcheck directories...")
   rcheck_dirs <- list.dirs(pkg, full.names = TRUE, recursive = FALSE)
   rcheck_dirs <- rcheck_dirs[grepl("\\.Rcheck$", rcheck_dirs)]
   unlink(rcheck_dirs, recursive = TRUE)
   
-  message("✅ All clean. Check complete.")
+  message("??? All clean. Check complete.")
 }
